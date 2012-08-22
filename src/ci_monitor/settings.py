3ef7a79faba52c7d66c9df74a74aa0d2d2f18b9f
Sam Mohamed <sam.mohamed@voltage.com>
2012-08-22 15:04:16 -0700

    upgraded app to use sqlite3

--- src/ci_monitor/settings.py
+++ src/ci_monitor/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ci_monitor',                      # Or path to database file if using sqlite3.
-        'USER': 'ci_monitor',                      # Not used with sqlite3.
-        'PASSWORD': '6boqN7qF',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': './db/ci_monitor.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
