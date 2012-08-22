db62628aed20464bcd62577792ec80d80327aedb
Sam Mohamed <sam.mohamed@voltage.com>
2012-08-22 09:17:18 -0700

    Finalized local installation of requirements

--- fabfile.py
+++ fabfile.py
@@ -5,16 +5,7 @@ from fabric import api
 """Globals"""
 
 app_name = 'ci_monitor'
-
-def _do_virtualenvwrapper_command(cmd):
-	""" This is tricky, because all virtualenwrapper commands are
-		actually bash functions, so we can't call them like we would other executables.
-	"""
-
-	out, err = subprocess.Popen(
-		['bash', '-c', '. /usr/local/bin/virtualenvwrapper.sh; %s' % cmd],stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-	print(out)
-	print(err)
+wrapper = '. /usr/local/bin/virtualenvwrapper.sh;'
 
 def install_requirements(*args,**kwargs):
 	#TODO: If need be, we can add a local pypi mirror for any additional packages
@@ -26,14 +17,13 @@ def install_requirements(*args,**kwargs):
 		api.run('%(install)s' % dict(install=install))
 	else:
 		#create virtual env
-		_do_virtualenvwrapper_command('mkvirtualenv %s' % app_name)
-		_do_virtualenvwrapper_command('workon %s' % app_name)
-		
-		#install requirements
+		mkvirtualenv = '%s %s' % (wrapper,'mkvirtualenv %s' % app_name)
+		workon = '%s %s' % (wrapper,'workon %s' % app_name)
 		install = '%s -e %s' % (install, os.getcwd())
-		api.local('%(install)s' % dict(
-				install = install
-			))
-			
+		
+		cmd = '%s ; %s ; %s' % (mkvirtualenv, workon, install)
+		
+		out, err = subprocess.Popen(cmd,shell=True).communicate()
+		
 def local(*args,**kwargs):
 	install_requirements()
