cd90c760ed1cffd99c3ec6e47f331e6d7f8b778c
Sam Mohamed <sam.mohamed@voltage.com>
2012-08-28 13:58:06 -0700

    added nohup to run django server

--- fabfile.py
+++ fabfile.py
@@ -44,7 +44,7 @@ def kill_django_dev_server(*args,**kwargs):
 		subprocess.call(shlex.split('kill -9 %s' % pid))
 		
 def start_django_dev_server(*args,**kwargs):
-	cmd = shlex.split('python ./src/ci_monitor/manage.py runserver 0.0.0.0:8000')
+	cmd = shlex.split('nohup python ./src/ci_monitor/manage.py runserver 0.0.0.0:8000 &')
 	
 	subprocess.Popen(cmd)
 	
