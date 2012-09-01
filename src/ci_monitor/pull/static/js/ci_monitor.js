74d320d81047130c234dc9fe233244b6ea3c926d
Sam Mohamed <sam.mohamed@voltage.com>
2012-09-01 15:01:59 -0700

    adjusted resize algorithm

--- src/ci_monitor/pull/static/js/ci_monitor.js
+++ src/ci_monitor/pull/static/js/ci_monitor.js
@@ -63,7 +63,7 @@ $(function(){
 	        curr_width = max_width;
 	        curr_height = max_width * ratio;
         } else if(curr_height >= max_height) {
-            curr_height = max_height;
+            curr_height = max_height * ratio;
             curr_width = curr_height / ratio;
         }
 	    
