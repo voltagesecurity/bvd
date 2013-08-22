ba3a3b780d62ad3d825a789e746f2064cba63bd8
NateGraff <nathaniel.graff@voltage.com>
2013-08-22 11:14:06 -0700

    Updated Edit Public TV modal to display jobs by product.

--- src/bvd/pull/static/js/edit_readonly_display.js
+++ src/bvd/pull/static/js/edit_readonly_display.js
@@ -1,6 +1,23 @@
-    var erd_display_line = function(pk, displayname, appletv_active) {
-        $erd_line = $("<div></div>");
+    var erd_display_line = function(pk, displayname, appletv_active, productname) {
+        /*
+            This function renders a job in the list of public display widgets in the Edit Public TV modal.
+
+            1. Creates a div or list element for the job
+            2. Adds the displayname to a span element
+            3. Creates a button
+            4. Assigns a callback to the button that changes the inactivity of the public display widget
+            5. Adds the job to either the 'no_product' div or passes it to the product renderer.
+        */
+        if(productname == 'no_product') {
+            productname = undefined;
+        }
+        if(productname != undefined) {
+            var $erd_line = $("<li></li>");
+        } else {
+            var $erd_line = $("<div></div>");
+        }
         $erd_line.addClass('erd_widget');
+        
         var primary_btn_text = "";
         if(!appletv_active) {
             $erd_line.addClass('erd_fade');
@@ -13,7 +30,7 @@
         $erd_line.append(label);
         
         var primary_btn = $('<div class="btn btn-primary erdd_primary_btn">' + primary_btn_text +
-                    '<div style="display:none;">' + pk + '</div>');
+                            '<div style="display:none;">' + pk + '</div></div>');
         $erd_line.append(primary_btn);
         primary_btn.on("click", function() {
             if(primary_btn_text == "Hide") {
@@ -29,10 +46,54 @@
             }
         });
 
-        $("#erd_display_widgets").append($erd_line);
+        if(productname != undefined) {
+            erd_display_product(productname, $erd_line);    
+        } else {
+            if($("#erd_display_widgets .no_product").length == 0) {
+                var widget_group = $("<div></div>");
+                widget_group.attr('class','no_product');
+                widget_group.addClass('erdd_widget_group');
+
+                // To simplify logic for removing products, creates a hidden div so that the
+                // product "garbage collector" just has to check if the product has <= 1 child
+                widget_group.append($("<div style='display:none'></div>"));
+
+                $("#erd_display_widgets").append(widget_group);
+            }
+            $("#erd_display_widgets .no_product").append($erd_line);
+        }
     };
 
+    var erd_display_product = function(productname, erd_line) {
+        /*
+            This function adds a job to a product in the Public Display widgets side of the Edit Public TV modal.
+
+            1. Creates a unordered list element for the product if it does not exist.
+            2. Adds the job to the prodcut.
+        */
+        if($("#erd_product_" + productname).length == 0) {
+            var product = $("<ul></ul>");
+            product.attr('id','erd_product_' + productname);
+            product.attr('class','erdd_widget_group');
+
+            var title = $("<li></li>");
+            title.attr('class', 'erdd_productname');
+            title.html(productname);
+
+            product.append(title);
+
+            $("#erd_display_widgets").append(product);
+        }
+        $("#erd_product_" + productname).append(erd_line);
+    }
+
     var change_erdd_line_active = function(pk, appletv_active) {
+        /*
+            This is the callback assigned to the button created in erd_display_line.
+
+            1. Posts data to the server to change the activity of the widget.
+            2. Updates the screen behind the modal.
+        */
         if(appletv_active == false) {
             BVD.utils.do_ajax('POST', '/pull/save_widget/',
                 {
@@ -57,13 +118,26 @@
     };
 
     var update_erdd = function() {
+        /*
+            This function updates the list of public display widgets in the Edit Public Display modal.
+
+            1. Pulls jobs from the display jobs view
+            2. Passes each job to the erd_display_line function to be drawn on the page
+            3. Checks all the product groupings to remove the ones that no longer have jobs ("garbage collector")
+        */
         BVD.utils.do_ajax('get', '/pull/pull_all_display_jobs',{}, function(data) {
             data = eval(data);
             $("#erd_display_widgets .erd_widget").remove();
-            $.each(data[0].jobs,function() {
-                $.each(this, function() {
-                    erd_display_line(this.pk, this.displayname, this.appletv_active);
+            $.each(data[0].jobs,function(productname, jobs) {
+                $.each(jobs, function() {
+                    erd_display_line(this.pk, this.displayname, this.appletv_active, productname);
                 });
+            });
+
+            $.each($(".erdd_widget_group"), function() {
+                if($(this).children().length <= 1) {
+                    $(this).remove();
+                }
             });
         });
     };
--- src/bvd/pull/views.py
+++ src/bvd/pull/views.py
@@ -388,13 +388,11 @@ def get_modal(request):
 
         product_widgets = []
         products = models.Product.objects.select_related().filter(jobs__user__username=request.user.username).distinct()
-        print products
         for product in products:
             product_widgets.append(dict(productname=product.productname, jobs=product.jobs.filter(entity_active=False).distinct()))
-        print product_widgets
 
         productless_widgets = models.UserCiJob.objects.filter(product__jobs__isnull=True,entity_active=False,user__username=request.user.username)
-        return render(request, 'inactive_widgets.html', dict(products=product_widgets, productless_widgets=productless_widgets))
+        return render(request, '%s.html' % template, dict(products=product_widgets, productless_widgets=productless_widgets))
 
     if template == "edit_readonly_display":
         if not request.user.is_authenticated():
@@ -402,9 +400,15 @@ def get_modal(request):
             return render_to_response(template,
                   dict(),
                   context_instance=RequestContext(request))
-        widgets = models.UserCiJob.objects.filter(user__username=request.user.username)
-        print widgets
-        return render(request, 'edit_readonly_display.html', dict(widgets=widgets))
+        
+
+        product_widgets = []
+        products = models.Product.objects.select_related().filter(jobs__user__username=request.user.username).distinct()
+        for product in products:
+            product_widgets.append(dict(productname=product.productname, jobs=product.jobs.filter(user__username=request.user.username)))
+
+        productless_widgets = models.UserCiJob.objects.filter(product__jobs__isnull=True,user__username=request.user.username)
+        return render(request, 'edit_readonly_display.html', dict(product_widgets=product_widgets, productless_widgets=productless_widgets))
        
     template = '%s.html' % template
     
